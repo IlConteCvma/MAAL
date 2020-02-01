f439bb3691edec2ef31885ff51a452f409e0f289
alessandroispw <56634853+alessandroispw@users.noreply.github.com>
2020-02-01 19:14:30 +0100

    [2] Miglioramento della struttura dei pacchetti. Aggiunta degli eventi all'interfaccia.

---
+++ src/logic/view/LoginGraphicController.java
@@ -0,0 +1,35 @@
+package logic.view;
+
+import javafx.application.Application;
+import javafx.fxml.*;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import logic.controller.LoginController;
+import javafx.event.*;
+import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
+
+public class LoginGraphicController{
+	
+	@FXML private PasswordField psw;
+	@FXML private TextField user;
+	
+	LoginController lg = new LoginController();
+	
+	@FXML
+	public void signIn(ActionEvent e) {
+		lg.findStudent(user.getText(),psw.getText());
+	}
+	
+	public void forgotPassword(ActionEvent e) {
+		System.out.println("Remember");
+	}
+	
+	public void signUp(ActionEvent e) {
+		System.out.println("Sign Up");
+	}
+
+}
