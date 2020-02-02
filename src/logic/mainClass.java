96acad9240580c5684fc8f0777561392868c5b29
IlConteCvma <45804966+IlConteCvma@users.noreply.github.com>
2020-02-02 18:17:16 +0100

    [3] Aggiunte le classi per HomePage

--- src/logic/mainClass.java
+++ src/logic/mainClass.java
@@ -11,7 +11,7 @@ public class MainClass extends Application{
 	
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("view/resources/LoginView.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("view/resources/HomeView.fxml"));
 		
 		Scene scene = new Scene(root);
 		stage.setScene(scene);
